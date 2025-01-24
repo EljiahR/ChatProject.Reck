36630e408f2b4050931202253fd89734a38d08b1
EljiahR <kcerhajile@gmail.com>
2025-01-24 15:24:32 -0500

    Changed how client receives chat history

--- ChatProject/Hubs/ChatHub.cs
+++ ChatProject/Hubs/ChatHub.cs
@@ -1,9 +1,11 @@
 using ChatProject.Models;
 using ChatProject.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
 
 namespace ChatProject.Hubs;
 
+[Authorize]
 public class ChatHub : Hub
 {
     private readonly IMessageService _service;
@@ -16,5 +18,12 @@ public class ChatHub : Hub
     {
         await _service.AddMessageAsync(new Message {Username = user, Content = content});
         await Clients.All.SendAsync("ReceiveMessage", user, content);
+    }
+
+    public override async Task OnConnectedAsync()
+    {
+        var messages = _service.GetAllMessagesAsync();
+
+        await Clients.Caller.SendAsync("ReceiveChatHistory", messages);
     }
 }
