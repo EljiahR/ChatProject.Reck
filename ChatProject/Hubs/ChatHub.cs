0ecded573c31eb9dbc404ac6b2572425105c6278
EljiahR <kcerhajile@gmail.com>
2025-01-30 17:04:43 -0500

    Added a check for channel claim in hub and update SendMessage to use group

--- ChatProject/Hubs/ChatHub.cs
+++ ChatProject/Hubs/ChatHub.cs
@@ -1,6 +1,7 @@
 using ChatProject.Models;
 using ChatProject.Services;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.SignalR;
 
 namespace ChatProject.Hubs;
@@ -14,10 +15,17 @@ public class ChatHub : Hub
     {
         _service = service;
     }
-    public async Task SendMessage(string user, string content)
+    
+    [Authorize(Policy = "BelongToChannel")]
+    public async Task SendMessage(string user, string content, string channelName)
     {
-        await _service.AddMessageAsync(new Message {Username = user, Content = content});
-        await Clients.All.SendAsync("ReceiveMessage", user, content);
+        if (IsInChannel(channelName))
+        {
+            await _service.AddMessageAsync(new Message {Username = user, Content = content});
+            await Clients.Group(channelName).SendAsync("ReceiveMessage", user, content, channelName);
+        }
+
+        await Clients.Caller.SendAsync("ReceiveMessage", "Server", "You are not a member of this channel");
     }
 
     public override async Task OnConnectedAsync()
@@ -25,5 +33,14 @@ public class ChatHub : Hub
         var messages = _service.GetAllMessagesAsync();
 
         await Clients.Caller.SendAsync("ReceiveChatHistory", messages);
+    }
+
+    public bool IsInChannel(string channelName)
+    {
+        var user = Context.User;
+        if (user == null) return false;
+
+        var userChannels = user.Claims.Where(x => x.Type == "Channel").Select(x => x.Value).ToList();
+        return userChannels.Contains(channelName);
     }
 }
