821d2e69cf7494e4940de29572c82b43ccf5fafe
EljiahR <kcerhajile@gmail.com>
2025-03-09 17:52:49 -0400

    Created custom relation table for channel and users

--- ChatProject/Models/JoinModels/ChannelUser.cs
+++ ChatProject/Models/JoinModels/ChannelUser.cs
@@ -1,6 +1,22 @@
+using ChatProject.Models.ChatChannelModels;
+using ChatProject.Models.ChatUserModels;
+
 namespace ChatProject.Models.JoinModels;
 
 public class ChannelUser
 {
+    public string UserId { get; set; }
+    public ChatUser User { get; set; }
     
+    public string ChannelId { get; set; }
+    public ChatChannel Channel { get; set; }
+    
+    public ChannelRole Role { get; set; }
+}
+
+public enum ChannelRole
+{
+    Creator,
+    Admin,
+    Member
 }
