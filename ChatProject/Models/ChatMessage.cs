749a765a172cc8a6006377be26c629a3a60b818f
EljiahR <kcerhajile@gmail.com>
2025-03-19 13:07:17 -0400

    Changed ChatMessage.cs SentAt to use Utc

--- ChatProject/Models/ChatMessage.cs
+++ ChatProject/Models/ChatMessage.cs
@@ -7,6 +7,6 @@ public class ChatMessage
     [MaxLength(50)] public string Id { get; set; } = Guid.NewGuid().ToString();
     public string Username { get; set; }
     public string Content { get; set; }
-    public DateTime SentAt { get; set; } = DateTime.Now;
+    public DateTime SentAt { get; set; } = DateTime.UtcNow;
     public string ChannelId { get; set; }
 }
