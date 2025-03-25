ff0c8074603ae4fa8236348994ff04a9523fdcf9
EljiahR <kcerhajile@gmail.com>
2025-03-25 18:23:44 -0400

    Setup relations with Friendship model in db context

--- ChatProject/Data/ChatDbContext.cs
+++ ChatProject/Data/ChatDbContext.cs
@@ -19,14 +19,17 @@ public class ChatDbContext : IdentityDbContext<ChatUser>
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
+        // Join table for User < - > Channel
         builder.Entity<ChannelUser>()
             .HasKey(cu => new { cu.UserId, cu.ChannelId });
 
+        // One-to-many: ChannelUser (join table) to User
         builder.Entity<ChannelUser>()
             .HasOne(cu => cu.User)
             .WithMany(u => u.ChannelUsers)
             .HasForeignKey(u => u.UserId);
 
+        // One-to-many: ChannelUser (join table) to Channel
         builder.Entity<ChannelUser>()
             .HasOne(cu => cu.Channel)
             .WithMany(c => c.ChannelUsers)
@@ -38,6 +41,20 @@ public class ChatDbContext : IdentityDbContext<ChatUser>
             .WithOne()
             .HasForeignKey(e => e.ChannelId)
             .IsRequired();
+
+        // Join table for User < - > User
+        builder.Entity<Friendship>()
+            .HasKey(f => new { f.InitiatorId, f.ReceiverId });
+
+        builder.Entity<Friendship>()
+            .HasOne(f => f.Initiator)
+            .WithMany(u => u.FriendsInitiated)
+            .HasForeignKey(f => f.InitiatorId);
+        
+        builder.Entity<Friendship>()
+            .HasOne(f => f.Receiver)
+            .WithMany(u => u.FriendsReceived)
+            .HasForeignKey(f => f.ReceiverId);
         
         base.OnModelCreating(builder);
     }
