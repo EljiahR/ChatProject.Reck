876e1ac4fbf0d28c3c659f6d88d3bffcf14f0a65
Elijah Reck <142916290+EljiahR@users.noreply.github.com>
2025-05-12 11:37:22 +0000

    Added a device property to the RefreshToken model

--- ChatProject/Models/RefreshToken.cs
+++ ChatProject/Models/RefreshToken.cs
@@ -5,6 +5,7 @@ public class RefreshToken
     public int Id { get; set; }
     public string Token { get; set; } = string.Empty;
     public string UserId { get; set; } = string.Empty;
+    public string Device { get; set; } = string.Empty;
     public DateTime ExpiresAt { get; set; }
     public bool IsRevoked { get; set; } = false;
 }
